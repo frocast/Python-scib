39c1fa9e2daacfa9949d42d13e659b8ac4703a88
Francisco Castillo <Castillo@MacBook-Pro-de-Francisco-2.local>
2017-07-12 18:26:10 -0500

    Tarea 3.4

--- Tareas/Obligatorias/Tarea_3_4.py
+++ Tareas/Obligatorias/Tarea_3_4.py
@@ -1,6 +1,10 @@
 from sklearn.neural_network import MLPClassifier
+from numpy.random import uniform, seed
+from matplotlib.mlab import griddata
+import matplotlib.pyplot as plt
+import numpy as np
 
-X = [git
+X = [
 
     [0,0],
     [0,1],
@@ -21,10 +25,27 @@ clf = MLPClassifier()
 # Ajusta los datos de entramiento para aprender
 clf.fit(X, Y)
 
-# En base a su aprendizaje predecimos
-# que pasa con el X = [1, 0, 1, 1]
-# (La respuesta fue [2] - Godinez)
+# make up data.
+#npts = int(raw_input('enter # of random points to plot:'))
+seed(0)
+npts = 100
+x = uniform(0, 1, npts)
+y = uniform(0, 1, npts)
+z = [clf.predict([[x[i], y[i]]])[0] for i in range(npts)]
 
-# print clf.predict([1, 0, 1, 1])
-
-print 'la Prediccion es: ' clf.predict([0, 1])
+# define grid.
+xi = np.linspace(0, 1, 500)
+yi = np.linspace(0, 1, 500)
+# grid the data.
+zi = griddata(x, y, z, xi, yi, interp='linear')
+# contour the gridded data, plotting dots at the nonuniform data points.
+CS = plt.contour(xi, yi, zi, 15, linewidths=0.5, colors='k')
+CS = plt.contourf(xi, yi, zi, 15,
+                  vmax=abs(zi).max(), vmin=-abs(zi).max())
+plt.colorbar()  # draw colorbar
+# plot data points.
+plt.scatter(x, y, marker='o', s=5, zorder=10)
+plt.xlim(0, 1)
+plt.ylim(0, 1)
+plt.title('griddata test (%d points)' % npts)
+plt.show()
