f0617599b892269a0701a87105eaba79193291f5
frocast <fra.castilloagmail.com>
2017-07-15 12:06:42 -0500

    ejemplos

---
+++ sesion_7/ejemplo_2.py
@@ -0,0 +1,12 @@
+import cv2 
+
+im = cv2.imread('balls.jpg', 0)
+
+k = cv2.waitKey(0)
+
+if k == 27:
+    cv2.imshow("Balls", im)    
+    cv2.destroyAllWindows()
+elif k == ord('s'):
+    cv2.imwrite('balls_g.png', im)
+
---
+++ sesion_7/ejemplo_3.py
@@ -0,0 +1,15 @@
+
+import numpy as np
+import cv2
+
+im = cv2.imread("balls.jpg", 1)
+(h, w, c) = im.shape
+
+x1 = int(0.20*w)
+y1 = int(0.85*h)
+
+x2 = int(0.75*w)
+y2 = int(0.35*h)
+
+im = cv2.line(im, (x1, y1), (x2, y2), (0,0,255), 32)
+cv2.imwrite("lineball.png", im)
